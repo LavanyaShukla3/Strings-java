2ad5d0e25c49b2ecb68d5bccbefe1122f878eeb9
LavanyaShukla3 <112153884+LavanyaShukla3@users.noreply.github.com>
2022-09-30 23:53:35 +0530

    Add files via upload
    Without loop only one word is captalized

---
+++ captalizeWord.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
+class captalizeWord{
+    public static void main(String[] args){
+        String s1 = "Sachin is a good guy";
+        
+         for (int i=0; i<s1.length(); i++)
+        {
+
+        System.out.println(s1.toUpperCase());
+        break;
+        }
+        
+        
+    }
+
+}
